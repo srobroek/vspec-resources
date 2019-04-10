b3aeb5061391a6284d9977c83d7a99f69ea1bdb3
Sjors Robroek <s.robroek@vxsan.com>
2019-04-10 19:35:53 +0400

    migration to vmware sdk

--- controls/example.rb
+++ controls/example.rb
@@ -4,16 +4,18 @@
 title 'sample section'
 
 # you can also use plain tests
-describe file('/tmp') do
-  it { should be_directory }
-end
+
 
 # you add controls here
 control 'tmp-1.0' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
-  title 'Create /tmp directory'             # A human-readable title
+  title 'hello'             # A human-readable title
   desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
-    it { should be_directory }
+describe vami('vcsa-01.lab.vxsan.com', auth: {user: 'root', pass: 'VMware1!VMware1!'}, ssl_verify: false) do
+
+    it { should exist }
+    its('ssh') { should cmp true }
+    its('ssodomain') { should cmp "vmware.com"}
   end
-end
+
+
